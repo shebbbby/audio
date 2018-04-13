ba03b1eb8ba92770dd7d7083349439a2ea8519df
Daniel Shebshayevich <danielshebshayevich@daniels-imac.attlocal.net>
2018-04-13 17:03:54 -0400

    made scraping process more efficient

--- scrapingProcess/fullProcess.js
+++ scrapingProcess/fullProcess.js
@@ -1,6 +1,68 @@
-// STEP 1
+// STEP 1 scrape intial data and declare initial variables -----------------------------
+
 var copyOfAllWordsArray = [];
 
+// Target the timecode that indicates the end of the audio.
+// THE FORMAT IS LIKE THIS: '00:00:00'
+var endOfAudio = document.querySelectorAll('.timecode')[1].innerText;
+
+var convertedTime = 0;
+convertedTime += Number(endOfAudio[7])*1;
+convertedTime += Number(endOfAudio[6])*10;
+convertedTime += Number(endOfAudio[4])*60;
+convertedTime += Number(endOfAudio[3])*1000;
+convertedTime += Number(endOfAudio[1])*10000;
+convertedTime += Number(endOfAudio[0]) *100000;
+endOfAudio = convertedTime;
+
+// GET TITLE OF TRANSCRIPT TEXT IN titleOfTranscript VARIABLE
+var titleOfTranscript = document.querySelector('.text--no-wrap').innerText;
+// GET ENTIRE TRANSCRIPT TEXT IN fullTranscript VARIABLE
+var fullTranscript = document.querySelector('.transcript').innerText;
+
+// Replace all line breaks with '!!!!' so that you can easily split into an array.
+var arrayOfLines = fullTranscript.replace( /\n/g, "!!!!" ).split( "!!!!" );
+
+// arrayOfTimeStamps will be an array of objects
+var arrayOfTimeStamps = [];
+
+for(var i = 0; i <=  arrayOfLines.length - 1; i++){
+// If a line has 8 ('00:00:00') or 17 ('00:00:00PlayPause') characters,
+  // it indicates that the line is a timestamp line
+  if(arrayOfLines[i].length === 8 || arrayOfLines[i].length === 17){
+    // Push those lines and the next paragraph into arrayOfTimeStamps
+    arrayOfTimeStamps.push({
+      timestamp: arrayOfLines[i].replace('PlayPause',''),
+      paragraph: arrayOfLines[i+1]
+    })
+  }
+}
+
+// Get the convertedTime of paragraphs, because they need to be comparable to the individual words.
+for(var i = 0; i <=  arrayOfTimeStamps.length - 1; i++){
+var convertedTime = 0;
+  convertedTime += Number(arrayOfTimeStamps[i].timestamp[7])/1;
+  convertedTime += Number(arrayOfTimeStamps[i].timestamp[6])*10;
+  convertedTime += Number(arrayOfTimeStamps[i].timestamp[4])*60;
+  convertedTime += Number(arrayOfTimeStamps[i].timestamp[3])*1000;
+  convertedTime += Number(arrayOfTimeStamps[i].timestamp[1])*10000;
+  convertedTime += Number(arrayOfTimeStamps[i].timestamp[0])*100000;
+  arrayOfTimeStamps[i].startsAt = convertedTime;
+}
+
+// Get when the paragraph ends.
+for(var i = 0; i <=  arrayOfTimeStamps.length - 1; i++){
+  if(arrayOfTimeStamps[i+1]){
+    arrayOfTimeStamps[i].endsAt = arrayOfTimeStamps[i+1].startsAt;
+  }else{
+    // For the last paragraph, the endsAt value will be the end of the audio
+    arrayOfTimeStamps[i].endsAt = endOfAudio;
+  }
+}
+
+
+
+// STEP 2 DECALARE FUNCTIONS THAT WILL BE USED IN SCROLLING INTERVAL -----------------------------------------
 function getAttributes ( node ) {
 var i,
     attributeNodes = node.attributes,
@@ -11,60 +73,6 @@ for ( i = 0; i < length; i++ ) attrs[attributeNodes[i].name] = attributeNodes[i]
 return attrs;
 }
 
-  var allSpans = document.querySelectorAll('span')
-
-
-  function getWordObject(word){
-    allInstancesArray = []
-      for(var i = 0; i < allSpans.length;i++){
-        if(allSpans[i].innerHTML === word){
-        var startsAt = Number(getAttributes(allSpans[i-2])["data-from"])/100;
-        var endAt = Number(getAttributes(allSpans[i-2])["data-to"])/100;
-        var confidence = getAttributes(allSpans[i-2])["class"].replace('word ','');
-        var dataKey = Number(getAttributes(allSpans[i-2])["data-key"]);
-        var dataOffsetKey = Number(getAttributes(allSpans[i-1])["data-key"]);
-        var previousWord = allSpans[i-3].innerHTML.replace(' ','');
-        var nextWord = allSpans[i+3].innerHTML.replace(' ','');
-
-        var isBeginningOfSentence = false;
-        var isLastWordInSentence = false;
-        if (previousWord.indexOf('.') !== -1) {
-          isBeginningOfSentence = true;
-        }
-        if (word[word.length-1] === '.') {
-          isLastWordInSentence = true;
-        }
-
-        allInstancesArray.push({
-          word:word,
-          startsAt: startsAt,
-          endAt: endAt,
-          confidence: confidence,
-          dataKey: dataKey,
-          dataOffsetKey: dataOffsetKey,
-          previousWord: previousWord,
-          nextWord: nextWord,
-          isBeginningOfSentence: isBeginningOfSentence,
-          isLastWordInSentence:isLastWordInSentence
-        })
-      }
-    }
-    copyOfAllWordsArray.push({
-      word: word.replace(/\s/g, ""),
-      objects: allInstancesArray
-    });
-    return allInstancesArray;
-  }
-
-  function getAllWordsTimestamps(){
-      for(var i = 0; i < allSpans.length;i++){
-        if(allSpans[i].innerHTML[0] === ' '){
-        getWordObject(allSpans[i].innerHTML)
-      }
-    }
-  };
-
-  getAllWordsTimestamps();
     // Removes duplicates in an array of objects
   function removeDuplicates(originalArray, prop) {
        var newArray = [];
@@ -96,8 +104,6 @@ return attrs;
      $(".textToCopyInput").remove();
 }
 
-  var copyOfAllWordsArray = removeDuplicates(copyOfAllWordsArray,'word');
-
    function isScrolledIntoView(elem){
         var docViewTop = $(window).scrollTop();
         var docViewBottom = docViewTop + $(window).height();
@@ -111,20 +117,6 @@ return attrs;
     function checkIfRowLineIsInView(){
       return isScrolledIntoView(document.querySelectorAll('.row .my-4')[1]);
     }
-
-
-//STEP 2 SCROLL THROUGH PAGE AND REPEATEDLY PASTE THIS CODE
-
-var scrollPosition = 100;
-
-var scrollInterval = null;
-
-scrollInterval = setInterval(function(){
-    scrollPosition += 500;
-    window.scrollTo(300, scrollPosition);
-
-    // get an array of all spans in document
-    var allSpans = document.querySelectorAll('span')
 
     function getWordObject(word){
     // allInstancesArray will be an array of objects with every instance of the word in the transcript
@@ -187,86 +179,43 @@ scrollInterval = setInterval(function(){
 
     };
 
+
+
+//STEP 3 SCROLL THROUGH PAGE AND REPEATED THIS CODE WITH setInterval---------------------------------
+
+// Start at position 100 on page
+var scrollPosition = 100;
+
+// Need to start as null to clearInterval in the future
+var scrollInterval = null;
+
+scrollInterval = setInterval(function(){
+  // every two seconds position will scroll down by 500.
+    scrollPosition += 500;
+    //  300 value is arbitrary.
+    window.scrollTo(300, scrollPosition);
+
+    // get an array of all spans in document
+    window.allSpans = document.querySelectorAll('span')
+
     getAllWordsTimestamps();
-
-
-
 
     // Since copyOfAllWordsArray will have many duplicates, remove all duplicates.
      copyOfAllWordsArray = removeDuplicates(copyOfAllWordsArray,'word');
 
+// IF YOU HAVE SCROLLED TO THE BOTTOM OF THE PAGE
     if(checkIfRowLineIsInView()){
         clearInterval(scrollInterval);
         console.log('Finished');
         // FINAL STEP COPY JSON
 
         // ADD TITLE OF VIDEO TO LAST POSITION IN copyOfAllWordsArray
-        copyOfAllWordsArray.push(document.querySelector('.text--no-wrap').innerText);
+        copyOfAllWordsArray.push(titleOfTranscript);
 
         // ADD FULL TRANSCRIPT TO LAST POSITION IN copyOfAllWordsArray
-        copyOfAllWordsArray.push(document.querySelector('.transcript').innerText);
+        copyOfAllWordsArray.push(fullTranscript);
 
-        // Target the timecode that indicates the end of the audio
-        var endOfAudio = document.querySelectorAll('.timecode')[1].innerText;
-
-        function endOfAudioConverter(){
-            var convertedTime = 0;
-            convertedTime += Number(endOfAudio[7])*1;
-            convertedTime += Number(endOfAudio[6])*10;
-            convertedTime += Number(endOfAudio[4])*60;
-            convertedTime += Number(endOfAudio[3])*1000;
-            convertedTime += Number(endOfAudio[1])*10000;
-            convertedTime += Number(endOfAudio[0]) *100000;
-            endOfAudio = convertedTime;
-        }
-
-        endOfAudioConverter();
-
-        // GET ENTIRE TRANSCRIPT TEXT IN string VARIABLE
-        var string = document.querySelector('.transcript').innerText;
-
-        // Replace all line breaks with '!!!!' so that you can easily split into an array.
-        var arrayOfLines = string.replace( /\n/g, "!!!!" ).split( "!!!!" );
-
-        // arrayOfTimeStamps will be an array of objects
-        var arrayOfTimeStamps = [];
-
-
-        for(var i = 0; i <=  arrayOfLines.length - 1; i++){
-          // If a line has 8 ('00:00:00') or 17 ('00:00:00PlayPause') characters,
-          // it indicates that the line is a timestamp line
-          if(arrayOfLines[i].length === 8 || arrayOfLines[i].length === 17){
-            // Push those lines and the next paragraph into arrayOfTimeStamps
-            arrayOfTimeStamps.push({
-              timestamp: arrayOfLines[i].replace('PlayPause',''),
-              paragraph: arrayOfLines[i+1]
-            })
-          }
-        }
-
-        // Get the convertedTime of paragraphs, because they need to be comparable to the individual words.
-        for(var i = 0; i <=  arrayOfTimeStamps.length - 1; i++){
-          var convertedTime = 0;
-            convertedTime += Number(arrayOfTimeStamps[i].timestamp[7])/1;
-            convertedTime += Number(arrayOfTimeStamps[i].timestamp[6])*10;
-            convertedTime += Number(arrayOfTimeStamps[i].timestamp[4])*60;
-            convertedTime += Number(arrayOfTimeStamps[i].timestamp[3])*1000;
-            convertedTime += Number(arrayOfTimeStamps[i].timestamp[1])*10000;
-            convertedTime += Number(arrayOfTimeStamps[i].timestamp[0])*100000;
-            arrayOfTimeStamps[i].startsAt = convertedTime;
-          }
-
-        // Get when the paragraph ends.
-        for(var i = 0; i <=  arrayOfTimeStamps.length - 1; i++){
-            if(arrayOfTimeStamps[i+1]){
-              arrayOfTimeStamps[i].endsAt = arrayOfTimeStamps[i+1].startsAt;
-            }else{
-              // For the last paragraph, the endsAt value will be the end of the audio
-              arrayOfTimeStamps[i].endsAt = endOfAudio;
-            }
-          }
-
-        // This is to link the paragraph with each individual word.
+        // This is to link each individual word with the correct paragraph
         for(var i = 0; i <= copyOfAllWordsArray.length - 1; i++){
           for(var y = 0; y <= copyOfAllWordsArray[i].objects.length - 1; y++){
             var startsAt = copyOfAllWordsArray[i].objects[y].startsAt;
@@ -286,5 +235,5 @@ scrollInterval = setInterval(function(){
 
 
 
-// AFTER COPYING THE ABOVE, TYPE THE BELOW SCRIPT.
+// AFTER COPYING THE ABOVE, TYPE THE BELOW SCRIPT. ---------------------------------------------
 // copyToClipboard(JSON.stringify(copyOfAllWordsArray));
